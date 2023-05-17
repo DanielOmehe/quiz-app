93dd574f446da280622a5f32374d11d249903ee8
DaniOmehe <danielomehe66@gmail.com>
2023-05-17 16:25:28 +0100

    Write logic for checking answer

--- data/questions.js
+++ data/questions.js
@@ -3,200 +3,200 @@ export const data = [
         question: 'Who is the founder of tesla?',
         correct_answer: 'Elon Musk',
         options: [
-            'Steve Jobs',
-            'Walter Anga',
-            'Catherine Kulman',
-            'Elon Musk'
+            { text: 'Steve Jobs', isCorrect: false },
+            { text: 'Walter Anga', isCorrect: false },
+            { text: 'Catherine Kulman', isCorrect: false },
+            { text: 'Elon Musk', isCorrect: true },
         ]
     },
     {
         question: 'Who was the first president of Nigeria?',
         correct_answer: 'Nnamdi Azikiwe',
         options: [
-            'Les Brown',
-            'Kyle Rogers',
-            'Nnamdi Azikiwe',
-            'Tafawa Balewa'
+            { text: 'Les Brown', isCorrect: false},
+            { text: 'Kyle Rogers', isCorrect: false},
+            { text: 'Nnamdi Azikiwe', isCorrect: true},
+            { text: 'Tafawa Balewa', isCorrect: false}
         ]
     },
     {
-        question: '"I am inevitable" who said this in the avengers movies "Endgame"?',
+        question: '\'I am inevitable\' who said this in the avengers movies "Endgame"?',
         correct_answer: 'Thanos',
         options: [
-            'Doctor Strange',
-            'Thanos',
-            'Loki',
-            'Thor'
+            { text: 'Doctor Strange', isCorrect: false },
+            { text: 'Thanos', isCorrect: true },
+            { text: 'Loki', isCorrect: false },
+            { text: 'Thor', isCorrect: false },
         ]
     },
     {
         question: 'Which of these actors has starred in an MCU movie?',
         correct_answer: 'Chris Pratt',
         options: [
-            'Chris Pratt',
-            'Freddie Kruger',
-            'Benny Blanco',
-            'Han Solo'
+            { text: 'Chris Pratt', isCorrect: true},
+            { text: 'Freddie Kruger', isCorrect: false},
+            { text: 'Benny Blanco', isCorrect: false},
+            { text: 'Han Solo', isCorrect: false}
         ]
     },
     {
         question: 'Who played the role of superman in the movie "Man of steel"?',
         correct_answer: 'Henry Cavill',
         options: [
-            'Jason Mamoa',
-            'Henry Cavill',
-            'Tom Hiddleston',
-            'Scarlet Johanssen'
+            { text: 'Jason Mamoa', isCorrect: false },
+            { text: 'Henry Cavill', isCorrect: true },
+            { text: 'Tom Hiddleston', isCorrect: false},
+            { text: 'Scarlet Johanssen', isCorrect: false}
         ]
     },
     {
         question: 'Who played the role of wonder woman in the movie "Justice League"?',
         correct_answer: 'Gal Gadot',
         options: [
-            'Emma Frost',
-            'Jennifer Lawrence',
-            'Priyanka Chopra',
-            'Gal Gadot'
+            { text: 'Emma Frost', isCorrect: false},
+            { text: 'Jennifer Lawrence', isCorrect: false},
+            { text: 'Priyanka Chopra', isCorrect: false},
+            { text: 'Gal Gadot', isCorrect: false}
         ]
     },
     {
         question: 'What is planet is regarded as the earth\'s twin?',
         correct_answer: 'Venus',
         options: [
-            'Saturn',
-            'Venus',
-            'Mars',
-            'Mercury'
+            { text: 'Saturn', isCorrect: false },
+            { text: 'Venus', isCorrect: true },
+            { text: 'Mars', isCorrect: false },
+            { text: 'Mercury', isCorrect: false },
         ]
     },
     {
         question: 'Who were the founders of HP computer company?',
         correct_answer: 'Paul Hewlett & David Packard',
         options: [
-            'Steve Jobs & Steve Wozniak',
-            'Larry Page & Sergey Brin',
-            'Freddie Kruger & Venom',
-            'Paul Hewlett & David Packard'
+            { text: 'Steve Jobs & Steve Wozniak', isCorrect: false },
+            { text: 'Larry Page & Sergey Brin', isCorrect: false },
+            { text: 'Freddie Kruger & Venom', isCorrect: false},
+            { text: 'Paul Hewlett & David Packard', isCorrect: true}
         ]
     },
     {
         question: 'What\'s the nae of our galaxy?',
         correct_answer: 'The Milky way',
         options: [
-            'The milky way',
-            'The spiral jungle',
-            'The big dipper',
-            'The little dipper'
+            { text: 'The milky way', isCorrect: true },
+            { text: 'The spiral jungle', isCorrect: false },
+            { text: 'The big dipper', isCorrect: false },
+            { text: 'The little dipper', isCorrect: false },
         ]
     },
     {
         question: 'Which of these is not a character from stranger things?',
         correct_answer: 'Billy Batson',
         options: [
-            'Will byers',
-            'Nancy Wheeler',
-            'Eleven',
-            'Billy Batson'
+            { text: 'Will byers', isCorrect: false},
+            { text: 'Nancy Wheeler', isCorrect: false},
+            { text: 'Eleven', isCorrect: false },
+            { text: 'Billy Batson', isCorrect: true },
         ]
     },
     {
         question: 'Which of these is not an african country?',
         correct_answer: 'Malaysia',
         options: [
-            'Malaysia',
-            'Seychelles',
-            'Rwanda',
-            'Namibia'
+            { text: 'Malaysia', isCorrect: true },
+            { text: 'Seychelles', isCorrect: false},
+            { text: 'Rwanda', isCorrect: false },
+            { text: 'Namibia', isCorrect: false }
         ]
     },
     {
         question: 'which of these is not a japanese state?',
         correct_answer: 'Wuhan',
         options: [
-            'Hiroshima',
-            'Wuhan',
-            'Nagasaki',
-            'Kyoto'
+            { text: 'Hiroshima', isCorrect: false },
+            { text: 'Wuhan', isCorrect: true },
+            {text: 'Nagasaki', isCorrect: false},
+            { text: 'Kyoto', isCorrect: false}
         ]
     },
     {
         question: 'Which of these is not one of the carribean islands?',
         correct_answer: 'Solomon Islands',
         options: [
-            'Solomon Islands',
-            'Hispaniola',
-            'The Bahamas',
-            'Cuba'
+            { text: 'Solomon Islands', isCorrect: true },
+            { text: 'Hispaniola', isCorrect: false},
+            { text: 'The Bahamas', isCorrect: false },
+            { text: 'Cuba', isCorrect: false}
         ]
     },
     {
         question: 'Who played captain Jack Sparrow in the movie \'Pirates of the carribean?',
         correct_answer: 'Johnny Depp',
         options: [
-            'Christopher Columbus',
-            'Johnny Depp',
-            'Will Byers',
-            'Jason Mamoa'
+            { text: 'Christopher Columbus', isCorrect: false},
+            { text: 'Johnny Depp', isCorrect: true},
+            { text: 'Will Byers', isCorrect: false},
+            { text: 'Jason Mamoa', isCorrect: false}
         ]
     },
     {
         question: 'Who was the first man to orbit the earth?',
         correct_answer: 'Yuri Gagarin',
         options: [
-            'Yuri Gagarin',
-            'Neil Armstrong',
-            'Nikola Tesla',
-            'Bill Gates'
+            { text:'Yuri Gagarin', isCorrect: true},
+            { text: 'Neil Armstrong', isCorrect: false},
+            { text: 'Nikola Tesla', isCorrect: false},
+            {text: 'Bill Gates', isCorrect: false}
         ]
     },
     {
         question: 'Who is the founder of Microsoft?',
         correct_answer: 'Bill Gates',
         options: [
-            'Bill Gates',
-            'Marco Veratti',
-            'Warren Buffet',
-            'Tiger Shroff'
+            { text: 'Bill Gates', isCorrect: true},
+            { text: 'Marco Veratti', isCorrect: false},
+            {text: 'Warren Buffet', isCorrect: false},
+            {text: 'Tiger Shroff', isCorrect: false}
         ]
     },
     {
         question: 'Which of these is not a european country?',
         correct_answer: 'Yemen',
         options: [
-            'Israel',
-            'Yemen',
-            'Belgium',
-            'Norway'
+            { text: 'Israel', isCorrect: false},
+            { text: 'Yemen', isCorrect: true},
+            { text: 'Belgium', isCorrect: false},
+            { text: 'Norway', isCorrect: false}
         ]
     },
     {
         question: 'In which of these countries can the Eiffel tower be found?',
         correct_answer: 'France',
         options: [
-            'UK',
-            'Scandinavia',
-            'France',
-            'Wales'
+            { text: 'UK', isCorrect: false},
+            { text: 'Scandinavia', isCorrect: false},
+            { text: 'France', isCorrect: true},
+            { text: 'Wales', isCorrect: false}
         ]
     },
     {
         question: 'Where is the statue of liberty located?',
         correct_answer: 'New York, USA',
         options: [
-            'New York, USA',
-            'Tokyo, Japan',
-            'London, England',
-            'Paris, France'
+            { text: 'New York, USA', isCorrect: true},
+            { text: 'Tokyo, Japan', isCorrect: false},
+            { text: 'London, England', isCorrect: false},
+            { text: 'Paris, France', isCorrect: false}
         ]
     },
     {
         question: 'Who is was the first man on the moon?',
         correct_answer: 'Neil Armstrong',
         options: [
-            'Yuri Gagarin',
-            'Thomas Edison',
-            'Michael Faraday',
-            'Neil Armstrong'
+            { text: 'Yuri Gagarin', isCorrect: false},
+            { text: 'Thomas Edison', isCorrect: false},
+            { text: 'Michael Faraday', isCorrect: false},
+            { text: 'Neil Armstrong', isCorrect: true}
         ]
     },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,118 @@
 import "./App.scss";
 import { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { data } from "../data/questions";
+
+const classnames = ['']
 
 function App() {
   const [quiz, setQuiz] = useState(data);
+  const [time, setTime] = useState(20);
   const [quizOver, setQuizOver] = useState(false);
-  const [correctAnswers,  setCorrectAnswers] = useState(0);
-  const [ score, setScore ] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  console.log(score);
 
-  // useEffect(() => {
-  //   const getQuizQuestions = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         "https://opentdb.com/api.php?amount=10"
-  //       );
-  //       if(response.data){
-  //         setQuestions(response.data.results);
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   getQuizQuestions();
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime((time) => time - 1);
+      if(time === 0){
+        setCurrentQuestion(currentQuestion => currentQuestion + 1)
+        setTime(20)
 
-  const handleClick =(index)=>{
-    setCurrentQuestion(index)
-  }
+        if(currentQuestion === quiz.length - 1){
+          setCurrentQuestion(0)
+          setQuizOver(true)
+        }
+      }
+    }, 900);
+    // const getQuizQuestions = async () => {
+    //   try {
+    //     const response = await axios.get(
+    //       'https://questions.aloc.ng/api/v2/q?subject=chemistry?api_key=.'
+    //     );
+    //     if(response.data){
+    //       setQuiz(response.data.results);
+    //     }
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+    // getQuizQuestions();
 
-  const checkAnswer =(answer, correct)=>{
-    if(answer === correct){
-      setScore(score => score + 5);
-      setCorrectAnswers(correctAnswers => correctAnswers + 1)
+    return () => clearTimeout(timer);
+  }, [time]);
+
+  const handleClick = (index) => {
+    setCurrentQuestion(index);
+    setTime(20)
+  };
+
+  const checkAnswer = (correct) => {
+    if (correct) {
+      setScore((score) => score + 5);
+      setCorrectAnswers((correctAnswers) => correctAnswers + 1);
+      setTime(20);
     }
-    if(currentQuestion === quiz.length - 1){
-      setQuizOver(true)
+    if (currentQuestion === quiz.length - 1) {
+      setQuizOver(true);
     }
-    setCurrentQuestion(currentQuestion + 1)
-  }
+    setCurrentQuestion(currentQuestion + 1);
+    setTime(20)
+  };
 
   return (
     <div className="app">
-      <div className="quiz-wrapper">{
-        quizOver ?
-        <div className="score">
-           You answered {correctAnswers} correctly out of {quiz.length} questions
-           Your Total score is: {score}
-        </div> : ( <>
-          <div className="quiz-container">
-          {quiz.map((quiz, id) => {
-            return (
-              <>
-                {id === currentQuestion ? (
-                  <div key={id} className="question">
-                    <h1>Question {id + 1}:</h1>
-                    <p>
-                      {quiz.question}
-                    </p>
-                    <hr />
-                    <ul className="options">
-                      {quiz.options.map((answer) => (
-                        <li key={answer} onClick={() => checkAnswer(answer, quiz.correct_answer)}>
-                          {answer}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ) : null}
-              </>
-            );
-          })}
-        </div>
-        <div className="numbers">
-          {quiz.map((question, index) => (
-            <button onClick={()=>handleClick(index)} className={index === currentQuestion ? 'active' : 'number'} key={index}>
-              {index + 1}
-            </button>
-          ))}
-        </div>
-  
-          </>)
-      }</div>
+      <div className="quiz-wrapper">
+        {quizOver ? (
+          <div className="score">
+            You answered {correctAnswers} correctly out of {quiz.length}{" "}
+            questions Your Total score is: {score}
+          </div>
+        ) : (
+          <>
+            <div className="quiz-container">
+              {quiz.map((quiz, id) => {
+                return (
+                  <>
+                    {id === currentQuestion ? (
+                      <div key={id} className="question">
+                        <div className="question-header">
+                          <h1 className="question-heading">Question {id + 1}:</h1>
+                          <p className="question-timer"> time left: {time}s </p>
+                        </div>
+                        <p className="question-text">{quiz.question}</p>
+                        <hr />
+                        <ul className="options">
+                          {quiz.options.map((answer) => (
+                            <li
+                              key={answer}
+                              onClick={() => checkAnswer(answer.isCorrect)}
+                            >
+                              {answer.text}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : null}
+                  </>
+                );
+              })}
+            </div>
+            <div className="numbers">
+              {quiz.map((question, index) => (
+                <button
+                  onClick={() => handleClick(index)}
+                  className={index === currentQuestion ? "active" : "number"}
+                  key={index}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
